2a6429701e3cfba0e8a74ef7deedb3750d4b581a
Milad Mahdavilayen <milad.mahdavilayen@kapsch.net>
2023-05-05 18:30:05 -0500

    gallery pics nwe

--- src/routes/gallary.jsx
+++ src/routes/gallary.jsx
@@ -7,11 +7,16 @@ import "../Gallery.css";
 function Gallery() {
 
   const images = [
-    { id: 1, src: "/static/media/images/rice.jpg", alt: "Image 1" },
-    { id: 2, src: "/static/media/images/rice (2).jpg", alt: "Image 2" },
-    { id: 3, src: "/static/media/images/rice (3).jpg", alt: "Image 3" },
-    { id: 4, src: "/static/media/images/rice (4).jpg", alt: "Image 4" },
-    { id: 5, src: "/static/media/images/rice (5).jpg", alt: "Image 5" }
+    { id: 1, src: "/static/media/images/dad_yahya.JPG", alt: "Image 6" },
+    { id: 2, src: "/static/media/images/dad_pointing.jpg", alt: "Image 7" },
+    { id: 3, src: "/static/media/images/dahat.jpg", alt: "Image 8" },
+    { id: 4, src: "/static/media/images/kooh.jpg", alt: "Image 9" },
+    { id: 5, src: "/static/media/images/layen.jpg", alt: "Image 10" },
+    { id: 6, src: "/static/media/images/rice.jpg", alt: "Image 1" },
+    { id: 7, src: "/static/media/images/rice (2).jpg", alt: "Image 2" },
+    { id: 8, src: "/static/media/images/rice (3).jpg", alt: "Image 3" },
+    { id: 9, src: "/static/media/images/rice (4).jpg", alt: "Image 4" },
+    { id: 10, src: "/static/media/images/rice (5).jpg", alt: "Image 5" }
   ];
 
   return (
