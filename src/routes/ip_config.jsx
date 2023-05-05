ff1cb29555a275314bce19202532032fa76dcf5d
Milad Mahdavilayen <milad.mahdavilayen@kapsch.net>
2023-05-05 15:25:20 -0500

    back

--- src/routes/ip_config.jsx
+++ src/routes/ip_config.jsx
@@ -15,8 +15,7 @@ function GetIp(props) {
     let hostname = '';
 
     // milad local ip address
-    // axios.get("https://ipinfo.io/35.146.15.101/json?token=401cc3d28ca774")
-    axios.get("https://ipinfo.io/35.146.15.101/json?token=401c")
+    axios.get("https://ipinfo.io/35.146.15.101/json?token=401cc3d28ca774")
       .then(response => {
         ip = response.data.ip;
         city = response.data.city;
